553ca5c568a4795635bc18a97b7237bbf4a68c3e
Sami <sami_b95@hotmail.fr>
2019-11-14 13:54:34 +0100

    Allow rows of different lengths

--- datalib.py
+++ datalib.py
@@ -1,8 +1,23 @@
 import pandas as pd
 
 def compute_means(filename):
-	df = pd.read_csv(filename, header=None)
-	return list(df.mean(axis=0))
+	means = []
+	available_values = []
+	with open(filename, "r") as f:
+		while True:
+			line = f.readline()
+			if line.strip() == "":
+				break
+			new_numbers = [ float(x.strip()) for x in line.split(",") ]
+			print ("new new_numbers: {}".format(new_numbers))
+			if len(new_numbers) > len(means):
+				available_values.extend([0] * (len(new_numbers) - len(means)))
+				means.extend([0] * (len(new_numbers) - len(means)))
+			for i in range(len(new_numbers)):
+				means[i] = (available_values[i] * means[i] + new_numbers[i]) / (available_values[i] + 1)
+			for i in range(len(new_numbers)):
+				available_values[i] += 1
+	return means, available_values
 
 def save_means(filename, means):
 	with open(filename, "w") as f:
